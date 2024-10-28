7cf523d1b32c96a7ce1d3c6e8f110e30e4b9b048
Ken <abcd90236@gmail.com>
2024-10-28 10:04:34 +0800

    online json viewer 查看資料型態 ，抓取資料

---
+++ lesson06/lesson06_00.py
@@ -0,0 +1,27 @@
+import requests
+
+
+url = 'https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON'
+
+response = requests.get(url)
+#print(type(response))   #type 為 Response
+
+data = response.json()
+# print(data,type(data))
+
+
+"""
+從online json viewer 去查看資料 可以看到
+這個檔案裏面有很多key
+其中要的內容在records裡面
+因此可以用以下方法來呼叫
+其中
+for items in data['records']
+這個 data['record'] 叫做 subscribe
+"""
+
+for items in data['records']:
+    print(items)
+
+
+
