48243a0dc4f5beae1db687e49b8a3871067dd5e2
Ken <abcd90236@gmail.com>
2024-12-02 09:13:27 +0800

    建立新的test1分支 -copy lesson13資料夾內容至 lesson14資料夾

---
+++ lesson14/lesson13_00.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+'''
+flask 就是一個支援 wsgi的應用程式
+真正支援wsgi的程式是 Gunicorn
+
+執行時，在終端機輸入
+flask --app 檔案名稱 run --debug
+
+'''
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template('index.j2')
+
+
+@app.route("/product")
+def product():
+    return render_template('product.j2')
