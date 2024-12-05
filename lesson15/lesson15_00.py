2265eefc7b445c09e75a03c13bd5a0567614e675
Ken <abcd90236@gmail.com>
2024-12-05 08:57:46 +0800

    lesson15_0

---
+++ lesson15/lesson15_00.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template
+import datasource
+'''
+flask 就是一個支援 wsgi的應用程式
+真正支援wsgi的程式是 Gunicorn
+
+執行時，在終端機輸入
+flask --app 檔案名稱 run --debug
+
+'''
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template('index.j2')
+
+
+@app.route("/product")
+def product():
+    cities:list[dict] = datasource.get_cities()
+    print(cities)
+    return render_template('product.j2')
+
+@app.route("/pricing")
+def pricing():
+    return render_template('pricing.j2')
+
+@app.route("/faqs")
+def faqs():
+    return render_template('faqs.j2')
+
+@app.route("/about")
+def about():
+    return render_template('about.j2')
