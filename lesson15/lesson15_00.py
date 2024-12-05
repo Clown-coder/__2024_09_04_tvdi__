bfc4a3eedf41134a104058a947353df2ea9f8511
Ken <abcd90236@gmail.com>
2024-12-05 11:35:52 +0800

    數據分類

--- lesson15/lesson15_00.py
+++ lesson15/lesson15_00.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import datasource
+
 '''
 flask 就是一個支援 wsgi的應用程式
 真正支援wsgi的程式是 Gunicorn
@@ -17,8 +18,16 @@ def index():
 @app.route("/product")
 def product():
     cities:list[dict] = datasource.get_cities()
-    # print(cities)
-    return render_template('product.j2',citys=cities)
+    page = request.args.get('page',1, type=int)
+    per_page = 9
+    start = (page-1) * per_page
+    end = start + per_page
+    total_pages = (len(cities) + per_page - 1 ) // per_page
+    items_on_page = cities[start:end]
+    return render_template('product.j2',
+                           items_on_page = items_on_page,
+                           total_pages = total_pages,
+                           page=page)
 
 @app.route("/pricing")
 def pricing():
