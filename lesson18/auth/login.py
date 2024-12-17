cbb94145c151bd6dcb4a9475e23b033b6459f3d1
Ken <abcd90236@gmail.com>
2024-12-17 11:33:54 +0800

    update new page

--- lesson18/auth/login.py
+++ lesson18/auth/login.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired,Length
 class MyForm(FlaskForm):
     email_field = fields.EmailField("Email address",validators=[DataRequired("必須要有資料")])
     password_field = fields.PasswordField("請輸入密碼",validators=[DataRequired("必須要有資料"),Length(5,10)])
-    e_paper_field = fields.BooleanField("訂閱電子報")
+    
     submit_field = fields.SubmitField("確認送出")
 
 @auth.route("/login",methods=['POST','GET'])
---
+++ lesson18/auth/registration.py
@@ -0,0 +1,5 @@
+from . import auth
+
+@auth.route('/regist')
+def regist():
+    return "<h1>註冊的1234678526365<h1>"
