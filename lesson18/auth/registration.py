6c4c9f2ae56d3eddb837539cd519ef5e8ab53921
Ken <abcd90236@gmail.com>
2024-12-17 11:43:29 +0800

    registrtion page

--- lesson18/auth/registration.py
+++ lesson18/auth/registration.py
@@ -1,5 +1,15 @@
 from . import auth
+from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import fields
+from wtforms.validators import DataRequired,Length
+
+class MyForm(FlaskForm):
+    email_field = fields.EmailField("Email address",validators=[DataRequired("必須要有資料")])
+    password_field = fields.PasswordField("請輸入密碼",validators=[DataRequired("必須要有資料"),Length(5,10)])
+    
+    submit_field = fields.SubmitField("確認送出")
 
 @auth.route('/regist')
 def regist():
-    return "<h1>註冊的1234678526365<h1>"
+    return render_template('/auth/registration.j2')
